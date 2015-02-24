c55fe406680c199b3c9887b325152cbd6417e99e
Oliver Friedmann <oliver.friedmann@gmail.com>
2015-02-24 20:31:28 +0100

    Demo/Test

--- tests/tests.js
+++ tests/tests.js
@@ -1,9 +1,24 @@
 $(document).ready(function () {
-	$("template").each(function () {
-		var template = $(this);
-		template.css("display", "none");
-		BetaJS.Browser.Loader.loadHtml(template.attr("src"), function (content) {
-			template.html(content);
-		});
+	$("democase").each(function () {
+		var democase = $(this);
+		democase.css("display", "none");
+		var src = democase.attr("src");
+		test(src, function () {
+			stop();
+			BetaJS.Browser.Loader.loadHtml(src, function (content) {
+				var parsed = $(content);
+				var template = "";
+				var code = "";
+				parsed.each(function () {
+					if (this.tagName == "TEST")
+						$("#qunit-fixture").html("<test>" + $(this).html() + "</test>");
+					if (this.tagName == "SCRIPT")
+						code = $(this).html();
+				});
+				var f = new Function (code + "test();");
+				f();
+				start();
+			});
+		})
 	});
 });

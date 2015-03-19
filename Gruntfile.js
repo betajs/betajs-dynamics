d98f1e46393a57b07aec4c93ff11c269f781060f
Oliver Friedmann <oliver.friedmann@gmail.com>
2015-03-19 17:32:08 -0400

    Closure Check, Dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,86 +2,122 @@ module.banner = '/*!\n<%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template
 
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-		pkg : grunt.file.readJSON('package.json'),
-		'revision-count': {
-		    options: {
-		      property: 'revisioncount',
-		      ref: 'HEAD'
-		    }
-		},
-		concat : {
-			options : {
-				banner : module.banner
-			},
-			dist_raw : {
-				dest : 'dist/betajs-dynamics-raw.js',
-				src : [
-					'src/fragments/begin.js-fragment',
-					'src/data/*.js',
-					'src/handlers/*.js',
-					'src/partials/*.js',
-					'src/dynamics/*.js',
-					'src/fragments/end.js-fragment',
-				]
-			},
-			dist_scoped: {
-				dest : 'dist/betajs-dynamics.js',
-				src : [
-				    'vendors/scoped.js',
-				    'dist/betajs-dynamics-noscoped.js'
-				]
-			}
-		},
-		preprocess : {
-			options: {
-			    context : {
-			    	MAJOR_VERSION: '<%= revisioncount %>',
-			    	MINOR_VERSION: (new Date()).getTime()
-			    }
-			},
-			dist : {
-			    src : 'dist/betajs-dynamics-raw.js',
-			    dest : 'dist/betajs-dynamics-noscoped.js'
-			}
-		},	
-		clean: ["dist/betajs-dynamics-raw.js"],
-		uglify : {
-			options : {
-				banner : module.banner
-			},
-			dist : {
-				files : {
-					'dist/betajs-dynamics-noscoped.min.js' : [ 'dist/betajs-dynamics-noscoped.js' ],					
-					'dist/betajs-dynamics.min.js' : [ 'dist/betajs-dynamics.js' ],					
+	grunt
+			.initConfig({
+				pkg : grunt.file.readJSON('package.json'),
+				'revision-count' : {
+					options : {
+						property : 'revisioncount',
+						ref : 'HEAD'
+					}
+				},
+				concat : {
+					options : {
+						banner : module.banner
+					},
+					dist_raw : {
+						dest : 'dist/betajs-dynamics-raw.js',
+						src : [ 'src/fragments/begin.js-fragment',
+								'src/data/*.js', 'src/handlers/*.js',
+								'src/partials/*.js', 'src/dynamics/*.js',
+								'src/fragments/end.js-fragment', ]
+					},
+					dist_scoped : {
+						dest : 'dist/betajs-dynamics.js',
+						src : [ 'vendors/scoped.js',
+								'dist/betajs-dynamics-noscoped.js' ]
+					}
+				},
+				preprocess : {
+					options : {
+						context : {
+							MAJOR_VERSION : '<%= revisioncount %>',
+							MINOR_VERSION : (new Date()).getTime()
+						}
+					},
+					dist : {
+						src : 'dist/betajs-dynamics-raw.js',
+						dest : 'dist/betajs-dynamics-noscoped.js'
+					}
+				},
+				clean : [ "dist/betajs-dynamics-raw.js",
+						"dist/betajs-dynamics-closure.js" ],
+				uglify : {
+					options : {
+						banner : module.banner
+					},
+					dist : {
+						files : {
+							'dist/betajs-dynamics-noscoped.min.js' : [ 'dist/betajs-dynamics-noscoped.js' ],
+							'dist/betajs-dynamics.min.js' : [ 'dist/betajs-dynamics.js' ],
+						}
+					}
+				},
+				shell : {
+					lint : {
+						command : "jsl +recurse --process ./src/*.js",
+						options : {
+							stdout : true,
+							stderr : true,
+						},
+						src : [ "src/*/*.js" ]
+					},
+					lintfinal : {
+						command : "jsl --process ./dist/betajs-dynamics.js",
+						options : {
+							stdout : true,
+							stderr : true,
+						},
+						src : [ "src/*/*.js" ]
+					}
+				},
+				closureCompiler : {
+					options : {
+						compilerFile : process.env.CLOSURE_PATH
+								+ "/compiler.jar",
+						compilerOpts : {
+							compilation_level : 'ADVANCED_OPTIMIZATIONS',
+							warning_level : 'verbose',
+							externs : [ "./src/fragments/closure.js-fragment" ]
+						}
+					},
+					dist : {
+						src : [ "./vendors/beta.js",
+								"./vendors/beta-browser-noscoped.js",
+								"./dist/betajs-dynamics-noscoped.js" ],
+						dest : "./dist/betajs-dynamics-closure.js"
+					}
+				},
+				wget : {
+					dependencies : {
+						options : {
+							overwrite : true
+						},
+						files : {
+							"./vendors/scoped.js" : "https://raw.githubusercontent.com/betajs/betajs-scoped/master/dist/scoped.js",
+							"./vendors/beta.js" : "https://raw.githubusercontent.com/betajs/betajs/master/dist/beta.js",
+							"./vendors/beta-browser-noscoped.js" : "https://raw.githubusercontent.com/betajs/betajs-browser/master/dist/beta-browser-noscoped.js",
+						}
+					}
 				}
-			}
-		},
-		shell: {
-			lint: {
-		    	command: "jsl +recurse --process ./src/*.js",
-		    	options: {
-                	stdout: true,
-                	stderr: true,
-            	},
-            	src: [
-            		"src/*/*.js"
-            	]
-			}
-		},
-	});
+			});
 
 	grunt.loadNpmTasks('grunt-newer');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-shell');	
+	grunt.loadNpmTasks('grunt-shell');
 	grunt.loadNpmTasks('grunt-git-revision-count');
 	grunt.loadNpmTasks('grunt-preprocess');
-	grunt.loadNpmTasks('grunt-contrib-clean');	
-	
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-wget');
+	grunt.loadNpmTasks('grunt-closure-tools');
+	grunt.loadNpmTasks('grunt-jsdoc');
 
-	grunt.registerTask('default', ['revision-count', 'concat:dist_raw', 'preprocess', 'clean', 'concat:dist_scoped', 'uglify']);
-	grunt.registerTask('lint', ['shell:lint']);	
-	grunt.registerTask('check', ['lint']);
+	grunt.registerTask('default', [ 'revision-count', 'concat:dist_raw',
+			'preprocess', 'clean', 'concat:dist_scoped', 'uglify' ]);
+	grunt.registerTask('lint', [ 'shell:lint', 'shell:lintfinal' ]);
+	grunt.registerTask('check', [ 'lint' ]);
+	grunt.registerTask('dependencies', [ 'wget:dependencies' ]);
+	grunt.registerTask('closure', [ 'closureCompiler', 'clean' ]);
 
 };
--- dist/betajs-dynamics-noscoped.js
+++ dist/betajs-dynamics-noscoped.js
@@ -1,5 +1,5 @@
 /*!
-betajs-dynamics - v0.0.1 - 2015-03-13
+betajs-dynamics - v0.0.1 - 2015-03-19
 Copyright (c) Oliver Friedmann,Victor Lingenthal
 MIT Software License.
 */
@@ -16,7 +16,7 @@ Scoped.binding("jquery", "global:jQuery");
 Scoped.define("module:", function () {
 	return {
 		guid: "d71ebf84-e555-4e9b-b18a-11d74fdcefe2",
-		version: '39.1426264477488'
+		version: '40.1426800668807'
 	};
 });
 

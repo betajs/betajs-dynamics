d5af0648704c6abf5115d26fec2cc4eb45786ec2
Oliver Friedmann <oliver.friedmann@gmail.com>
2015-05-06 16:08:51 -0400

    Merge

--- dist/betajs-dynamics.js
+++ dist/betajs-dynamics.js
@@ -554,7 +554,7 @@ Scoped.binding("jquery", "global:jQuery");
 Scoped.define("module:", function () {
 	return {
 		guid: "d71ebf84-e555-4e9b-b18a-11d74fdcefe2",
-		version: '64.1430940034137'
+		version: '68.1430942921059'
 	};
 });
 

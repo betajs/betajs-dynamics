4bb8be09308db01c2a6ac6efd1dca1dd4438dd2d
Victor Lingenthal <victor.lingenthal@gmail.com>
2015-11-30 23:32:28 +0100

    Event Partial Demo updated for browser use

--- dist/betajs-dynamics.js
+++ dist/betajs-dynamics.js
@@ -577,7 +577,7 @@ Scoped.binding("jquery", "global:jQuery");
 Scoped.define("module:", function () {
 	return {
 		guid: "d71ebf84-e555-4e9b-b18a-11d74fdcefe2",
-		version: '178.1448921763990'
+		version: '181.1448921787371'
 	};
 });
 

71d412ed6c14ad6559a8b89cd46aa528d01d12cf
Oliver Friedmann <oliver.friedmann@gmail.com>
2015-11-29 20:09:46 -0500

    Preserve Types Test; Event Partial

--- dist/betajs-dynamics.js
+++ dist/betajs-dynamics.js
@@ -1,5 +1,5 @@
 /*!
-betajs-dynamics - v0.0.13 - 2015-11-28
+betajs-dynamics - v0.0.14 - 2015-11-29
 Copyright (c) Oliver Friedmann,Victor Lingenthal
 MIT Software License.
 */
@@ -560,7 +560,7 @@ Public.exports();
 }).call(this);
 
 /*!
-betajs-dynamics - v0.0.13 - 2015-11-28
+betajs-dynamics - v0.0.14 - 2015-11-29
 Copyright (c) Oliver Friedmann,Victor Lingenthal
 MIT Software License.
 */
@@ -577,7 +577,7 @@ Scoped.binding("jquery", "global:jQuery");
 Scoped.define("module:", function () {
 	return {
 		guid: "d71ebf84-e555-4e9b-b18a-11d74fdcefe2",
-		version: '171.1448746608628'
+		version: '172.1448845769969'
 	};
 });
 
@@ -2173,6 +2173,27 @@ Scoped.define("module:Partials.ClickPartial", ["module:Handlers.Partial"], funct
  		};
  	});
  	Cls.register("ba-click");
+	return Cls;
+});
+
+Scoped.define("module:Partials.EventPartial", ["module:Handlers.Partial"], function (Partial, scoped) {
+  	var Cls = Partial.extend({scoped: scoped}, function (inherited) {
+ 		return {
+			
+ 			bindTagHandler: function (handler) {
+ 				handler.on(this.__postfix, function (arg1, arg2, arg3, arg4) {
+ 					this._node._handler.call(this._value, arg1, arg2, arg3, arg4);
+ 				}, this);
+ 			},
+
+ 			constructor: function (node, args, value, postfix) {
+ 				inherited.constructor.apply(this, arguments);
+ 				this.__postfix = postfix;
+ 			}
+ 		
+ 		};
+ 	});
+ 	Cls.register("ba-event");
 	return Cls;
 });
 

9341f07562bf01454006ba8dd0f67c34239d2cf6
Oliver Friedmann <oliver.friedmann@gmail.com>
2015-12-12 16:00:16 -0500

    Bump Scoped

--- dist/betajs-dynamics-noscoped.js
+++ dist/betajs-dynamics-noscoped.js
@@ -1,5 +1,5 @@
 /*!
-betajs-dynamics - v0.0.21 - 2015-12-12
+betajs-dynamics - v0.0.22 - 2015-12-12
 Copyright (c) Oliver Friedmann,Victor Lingenthal
 MIT Software License.
 */
@@ -16,10 +16,12 @@ Scoped.binding("jquery", "global:jQuery");
 Scoped.define("module:", function () {
 	return {
 		guid: "d71ebf84-e555-4e9b-b18a-11d74fdcefe2",
-		version: '194.1449936033018'
+		version: '195.1449953977286'
 	};
 });
 
+Scoped.assumeVersion("base:version", 444);
+Scoped.assumeVersion("browser:version", 58);
 Scoped.define("module:Data.Mesh", [
 	    "base:Class",
 	    "base:Events.EventsMixin",
